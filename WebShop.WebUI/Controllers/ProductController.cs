7451a60fd175a74bccfd3ed5ccba2d311a9bf9fd
Laurent Luypaert <laur.luyp@hotmail.com>
2019-06-07 11:34:21 +0200

    Added folders, controllers and views .

---
+++ WebShop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webshop.Core.Models;
+using WebShop.DataAccess.InMemory.Repositories;
+
+namespace WebShop.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductRepository Context { get; set; } = new ProductRepository();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = Context.Collection().ToList();
+
+            return View(products);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var product = new Product();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            Context.Insert(product);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
