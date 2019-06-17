119ef9bbf226ef6fd5ee04b40dc06ddfb331caa6
Laurent Luypaert <laur.luyp@hotmail.com>
2019-06-17 11:24:11 +0200

    Added details

--- WebShop.WebUI/Controllers/HomeController.cs
+++ WebShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webshop.Core.Models;
+using WebShop.Core.Interfaces.Repositories;
 
 namespace WebShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> Context;
+        IRepository<ProductCatergory> ProductCategoryContext;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCatergory> productCategoryContext)
+        {
+            this.Context = productContext;
+            this.ProductCategoryContext = productCategoryContext;
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+
+            List<Product> products = Context.Collection().ToList();
+            return View(products);
         }
 
         public ActionResult About()
@@ -25,6 +39,17 @@ namespace WebShop.WebUI.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Details(string  id)
+        {
+            var product = Context.FindById(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(product);
         }
     }
 }
